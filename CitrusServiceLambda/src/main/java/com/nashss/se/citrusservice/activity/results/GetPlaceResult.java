d17bcafe4a910a23bf8b90804ef391d68ecef0b8
Noah Cain <nzcain30@gmail.com>
2023-06-05 15:46:05 -0500

    working on placeDAO

--- CitrusServiceLambda/src/main/java/com/nashss/se/citrusservice/activity/results/GetPlaceResult.java
+++ CitrusServiceLambda/src/main/java/com/nashss/se/citrusservice/activity/results/GetPlaceResult.java
@@ -1,4 +1,37 @@
 package com.nashss.se.citrusservice.activity.results;
+import com.nashss.se.citrusservice.models.PlaceModel;
+
 
 public class GetPlaceResult {
+    private final PlaceModel placeModel;
+
+    public GetPlaceResult(PlaceModel placeModel){
+        this.placeModel = placeModel;
+    }
+    public PlaceModel getPlacesModel(){
+        return placeModel;
+    }
+
+    @Override
+    public String toString() {
+        return "GetPlaceResult{" +
+                "placesModel=" + placeModel +
+                '}';
+    }
+    public static class Builder{
+        private PlaceModel placeModel;
+
+        public Builder withPlaceModel(PlaceModel placeModel){
+            this.placeModel = placeModel;
+            return this;
+        }
+
+        public GetPlaceResult build(){
+            return new GetPlaceResult(placeModel);
+        }
+    }
+    public static Builder builder(){
+        return new Builder();
+    }
+
 }
---
+++ CitrusServiceLambda/src/main/java/com/nashss/se/citrusservice/lambda/GetPlaceLambda.java
@@ -0,0 +1,26 @@
+package com.nashss.se.citrusservice.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.nashss.se.citrusservice.activity.requests.GetPlaceRequest;
+import com.nashss.se.citrusservice.activity.results.GetPlaceResult;
+
+public class GetPlaceLambda extends LambdaActivityRunner<GetPlaceRequest, GetPlaceResult> implements RequestHandler<LambdaRequest<GetPlaceRequest>,LambdaResponse> {
+    /**
+     * Handles a Lambda Function request
+     *
+     * @param input   The Lambda Function input
+     * @param context The Lambda execution environment context object.
+     * @return The Lambda Function output
+     */
+    @Override
+    public LambdaResponse handleRequest(LambdaRequest<GetPlaceRequest> input, Context context) {
+        return super.runActivity(
+                () -> input.fromPath(path ->
+                        GetPlaceRequest.builder()
+                                .withId(path.get("placeId"))
+                                .build()),
+                (request,serviceComponent) ->
+                        serviceComponent.provideGetPlaceActivity().handleRequest(request));
+    }
+}
