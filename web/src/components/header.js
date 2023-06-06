ae3de84df51c3e5e041bb7de89296bb79df73385
Noah Cain <nzcain30@gmail.com>
2023-06-06 15:53:26 -0500

    WIP place endpoints

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -9,9 +9,7 @@ export default class Header extends BindingClass {
         super();
 
         const methodsToBind = [
-            'addHeaderToPage', 'createSiteTitle', 'createUserInfoForHeader',
-            'createLoginButton', 'createLoginButton', 'createLogoutButton'
-        ];
+            'addHeaderToPage'];
         this.bindClassMethods(methodsToBind, this);
 
         this.client = new CitrusClient();
@@ -31,50 +29,5 @@ export default class Header extends BindingClass {
         header.appendChild(userInfo);
     }
 
-    createSiteTitle() {
-        const homeButton = document.createElement('a');
-        homeButton.classList.add('header_home');
-        homeButton.href = 'index.html';
-        homeButton.innerText = 'Playlists';
-
-        const siteTitle = document.createElement('div');
-        siteTitle.classList.add('site-title');
-        siteTitle.appendChild(homeButton);
-
-        return siteTitle;
-    }
-
-    createUserInfoForHeader(currentUser) {
-        const userInfo = document.createElement('div');
-        userInfo.classList.add('user');
-
-        const childContent = currentUser
-            ? this.createLogoutButton(currentUser)
-            : this.createLoginButton();
-
-        userInfo.appendChild(childContent);
-
-        return userInfo;
-    }
-
-    createLoginButton() {
-        return this.createButton('Login', this.client.login);
-    }
-
-    createLogoutButton(currentUser) {
-        return this.createButton(`Logout: ${currentUser.name}`, this.client.logout);
-    }
-
-    createButton(text, clickHandler) {
-        const button = document.createElement('a');
-        button.classList.add('button');
-        button.href = '#';
-        button.innerText = text;
-
-        button.addEventListener('click', async () => {
-            await clickHandler();
-        });
-
-        return button;
-    }
+  
 }
