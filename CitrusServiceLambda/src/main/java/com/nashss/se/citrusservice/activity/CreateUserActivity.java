88515a4d691590004a0277bc7d900d6f6106fe5c
Noah Cain <nzcain30@gmail.com>
2023-06-07 20:20:39 -0500

    Almost done with create-profile

--- CitrusServiceLambda/src/main/java/com/nashss/se/citrusservice/activity/CreateUserActivity.java
+++ CitrusServiceLambda/src/main/java/com/nashss/se/citrusservice/activity/CreateUserActivity.java
@@ -5,12 +5,13 @@ import com.nashss.se.citrusservice.activity.results.CreateUserResult;
 import com.nashss.se.citrusservice.converters.ModelConverter;
 import com.nashss.se.citrusservice.dynamodb.UserDao;
 import com.nashss.se.citrusservice.dynamodb.models.User;
+import com.nashss.se.citrusservice.exceptions.InvalidAttributeException;
 import com.nashss.se.citrusservice.models.UserModel;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.inject.Inject;
-import javax.management.InvalidAttributeValueException;
+
 
 public class CreateUserActivity {
     private final Logger log = LogManager.getLogger();
@@ -20,12 +21,12 @@ public class CreateUserActivity {
     public CreateUserActivity(UserDao userDao){
         this.userDao = userDao;
     }
-    public CreateUserResult handleRequest(final CreateUserRequest createUserRequest) throws InvalidAttributeValueException {
+    public CreateUserResult handleRequest(final CreateUserRequest createUserRequest)  {
         log.info("Received CreateUserRequest{}", createUserRequest);
 
         if(createUserRequest.getFirstName() == null ||
                 createUserRequest.getFirstName().isEmpty()){
-            throw new InvalidAttributeValueException("Name cannot contain illegal characters");
+            throw new InvalidAttributeException("Name cannot contain invalid characters");
         }
         User newUser = userDao.saveUser(true,
                 createUserRequest.getUserId(),
--- CitrusServiceLambda/src/main/java/com/nashss/se/citrusservice/lambda/CreateUserLambda.java
+++ CitrusServiceLambda/src/main/java/com/nashss/se/citrusservice/lambda/CreateUserLambda.java
@@ -1,4 +1,35 @@
 package com.nashss.se.citrusservice.lambda;
 
-public class CreateUserLambda {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.nashss.se.citrusservice.activity.requests.CreateUserRequest;
+import com.nashss.se.citrusservice.activity.results.CreateUserResult;
+
+public class CreateUserLambda extends LambdaActivityRunner<CreateUserRequest, CreateUserResult> implements RequestHandler<AuthenticatedLambdaRequest<CreateUserRequest>,LambdaResponse> {
+    /**
+     * Handles a Lambda Function request
+     *
+     * @param input   The Lambda Function input
+     * @param context The Lambda execution environment context object.
+     * @return The Lambda Function output
+     */
+    @Override
+    public LambdaResponse handleRequest(AuthenticatedLambdaRequest<CreateUserRequest> input, Context context) {
+        return super.runActivity(
+                () -> {
+                    CreateUserRequest unauthenticatedRequest = input.fromBody(CreateUserRequest.class);
+                    return input.fromUserClaims(claims ->
+                            CreateUserRequest.builder()
+                                    .withUserId(claims.get("userId"))
+                                    .withFirstName(unauthenticatedRequest.getFirstName())
+                                    .withLastName(unauthenticatedRequest.getLastName())
+                                    .withGender(unauthenticatedRequest.getGender())
+                                    .withDateOfBirth(unauthenticatedRequest.getDateOfBirth())
+                                    .withUserInterests(unauthenticatedRequest.getUserInterests())
+                                    .build());
+                },
+                (request, serviceComponent) ->
+                        serviceComponent.provideCreateUserActivity().handleRequest(request)
+        );
+    }
 }
