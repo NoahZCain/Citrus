72e198ba6341acf458741970966658ae6b83c31a
Noah Cain <nzcain30@gmail.com>
2023-06-18 12:54:21 -0500

    Request done

---
+++ CitrusServiceLambda/src/main/java/com/nashss/se/citrusservice/activity/requests/RemoveAccessibilityTagsRequest.java
@@ -0,0 +1,53 @@
+package com.nashss.se.citrusservice.activity.requests;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import java.util.Set;
+
+@JsonDeserialize(builder = RemoveAccessibilityTagsRequest.Builder.class)
+public class RemoveAccessibilityTagsRequest {
+    private final String placeId;
+    private final Set<String> tagsToRemove;
+
+    private RemoveAccessibilityTagsRequest(String placeId, Set<String> tagsToRemove){
+        this.placeId = placeid;
+        this.tagsToRemove = tagsToRemove;
+    }
+
+    public String getPlaceId() {
+        return placeId;
+    }
+
+    public Set<String> getTagsToRemove() {
+        return tagsToRemove;
+    }
+
+    @Override
+    public String toString() {
+        return "RemoveAccessibilityTagsRequest{" +
+                "placeId='" + placeId + '\'' +
+                ", tagsToRemove=" + tagsToRemove +
+                '}';
+    }
+    public static Builder builder(){
+        return new Builder();
+    }
+
+    public static class Builder{
+        private String placeId;
+        private Set<String> tagsToRemove;
+
+        public Builder withPlaceId(String placeId){
+            this.placeId = placeId;
+            return this;
+        }
+        public Builder withTagsToRemove(Set<String> tagsToRemove){
+            this.tagsToRemove = tagsToRemove;
+            return this;
+        }
+
+        public RemoveAccessibilityTagsRequest build(){
+            return new RemoveAccessibilityTagsRequest(placeId,tagsToRemove);
+        }
+    }
+}
