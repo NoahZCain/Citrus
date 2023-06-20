23e0069b3c8b5faef0edb2a3d889ed204bb5bbfd
Noah Cain <nzcain30@gmail.com>
2023-06-19 19:33:34 -0500

    Edit Profile done, coming back to style

--- web/src/pages/editUser.js
+++ web/src/pages/editUser.js
@@ -1,8 +1,11 @@
 import CitrusClient from "../api/citrusClient";
+import BindingClass from "./util/bindingClass";
 import DataStore from "./util/DataStore";
 
-class EditUser {
+class EditUser extends BindingClass {
   constructor() {
+    super();
+    this.bindClassMethods(["clientLoaded", "updateUser", "mount", "populateForm"], this);
     this.dataStore = new DataStore();
     this.client = new CitrusClient();
   }
@@ -10,44 +13,73 @@ class EditUser {
   async clientLoaded() {
     const identity = await this.client.getIdentity();
     const user = await this.client.getUser(identity.email);
-    const name = identity.name;
+    const firstName = user.userModel.firstName;
+    const lastName = user.userModel.lastName;
     const email = identity.email;
     const gender = user.userModel.gender;
     const dob = user.userModel.dateOfBirth;
     const userInterests = user.userModel.interests;
     this.dataStore.set("email", email);
-    this.dataStore.set("name", name);
+    this.dataStore.set("firstName", firstName);
+    this.dataStore.set("lastName", lastName);
     this.dataStore.set("gender", gender);
     this.dataStore.set("dateOfBirth", dob);
     this.dataStore.set("interests", userInterests);
   }
 
   async updateUser() {
-    const name = document.getElementById("name").value;
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
+    const name = firstName + " " + lastName;
     const gender = document.getElementById("gender").value;
-    const dob = document.getElementById("dateOfBirth").value;
-    const interests = document.getElementById("interests").value;
-
-    this.dataStore.set("name", name);
-    this.dataStore.set("gender", gender);
-    this.dataStore.set("dateOfBirth", dob);
-    this.dataStore.set("interests", interests);
-
-    const email = this.dataStore.get("email");
+    const dateOfBirth = document.getElementById("dateOfBirth").value;
+    const interests = document.getElementById("interests").value.split(",").map((interest) => interest.trim());
+    const userId = this.dataStore.get("email");
+  
     const user = {
-      userModel: {
-        firstName: name.split(" ")[0],
-        lastName: name.split(" ")[1],
-        gender,
-        dateOfBirth: dob,
-        interests: interests.split(","),
-      },
+      userId,
+      firstName,
+      lastName,
+      gender,
+      dateOfBirth,
+      userInterests: interests,
     };
-
-    await this.client.updateUser(email, user);
-
-    // Redirect to the view user page after updating
-    window.location.href = '/viewUser.html';
+  
+    await this.client.updateUser(userId, user);
+  
+    // Fetch the updated user data from the server
+    const updatedUser = await this.client.getUser(userId);
+  
+    // Update the stored values in the EditUser class
+    const updatedFirstName = updatedUser.firstName;
+    const updatedLastName = updatedUser.lastName;
+    const updatedGender = updatedUser.gender;
+    const updatedDateOfBirth = updatedUser.dateOfBirth;
+    const updatedInterests = updatedUser.userInterests;
+  
+    this.dataStore.set("firstName", updatedFirstName);
+    this.dataStore.set("lastName", updatedLastName);
+    this.dataStore.set("gender", updatedGender);
+    this.dataStore.set("dateOfBirth", updatedDateOfBirth);
+    this.dataStore.set("interests", updatedInterests);
+  
+    // Redirect to the profile page after updating
+    window.location.href = "/profile.html";
+  }
+  
+  
+  populateForm() {
+    const firstName = this.dataStore.get("firstName");
+    const lastName = this.dataStore.get("lastName");
+    const gender = this.dataStore.get("gender");
+    const dob = this.dataStore.get("dateOfBirth");
+    const interests = this.dataStore.get("interests");
+  
+    document.getElementById("firstName").value = firstName || "";
+    document.getElementById("lastName").value = lastName || "";
+    document.getElementById("gender").value = gender || "";
+    document.getElementById("dateOfBirth").value = dob || "";
+    document.getElementById("interests").value = interests ? interests.join(", ") : "";
   }
 
   mount() {
@@ -61,22 +93,13 @@ class EditUser {
     });
   }
 
-  populateForm() {
-    const name = this.dataStore.get("name");
-    const gender = this.dataStore.get("gender");
-    const dob = this.dataStore.get("dateOfBirth");
-    const interests = this.dataStore.get("interests");
-
-    document.getElementById("name").value = name || "";
-    document.getElementById("gender").value = gender || "";
-    document.getElementById("dateOfBirth").value = dob || "";
-    document.getElementById("interests").value = interests ? interests.join(", ") : "";
-  }
 }
 
 const main = async () => {
   const editUser = new EditUser();
+  await editUser.clientLoaded();
+  editUser.populateForm();
   editUser.mount();
 };
 
-window.addEventListener('DOMContentLoaded', main);
+window.addEventListener("DOMContentLoaded", main);
--- web/src/pages/viewUser.js
+++ web/src/pages/viewUser.js
@@ -3,128 +3,128 @@ import Header from "../components/header";
 import DataStore from "./util/DataStore";
 import BindingClass from "./util/bindingClass";
 
-class ViewUser extends BindingClass{
-    constructor(){
-        super();
-        this.bindClassMethods(['clientLoaded','mount','logout','redirectHomePage','addName','addEmail',
-        ,'addGender', 'addInterests', 'addDateOfBirth','redirectEditUser'],this);
-        this.dataStore = new DataStore;
-        this.client = new CitrusClient();
-        this.Header = new Header(this.dataStore);
-    }
-    async clientLoaded() {
-        const identity = await this.client.getIdentity();
-        const user = await this.client.getUser(identity.email);
-        const name = identity.name;
-        const email = identity.email;
-        const gender = user.userModel.gender;
-        const dob = user.userModel.dateOfBirth;
-        const userInterests = user.userModel.interests;
-        this.dataStore.set("email", email);
-        this.dataStore.set("name", name);
-        this.dataStore.set("interests", userInterests);
-        this.dataStore.set("gender",gender);
-        this.dataStore.set("dateOfBirth",dob);
-        console.log(dob);
-      }
-    mount(){
-        // document.getElementById('logout').addEventListener('click',this.redirectHomePage);
-        this.client = new CitrusClient();
-        
-        document.getElementById('logout').addEventListener('click',this.logout);
-        this.clientLoaded().then(() => {
-            this.addName();
-            this.addEmail();
-            this.addInterests();
-            this.addGender();
-            this.addDateOfBirth();
-            
-        });
-    }
-     redirectHomePage(){
-        window.location.href = '/landingPage.html';
-    }
+class ViewUser extends BindingClass {
+  constructor() {
+    super();
+    this.bindClassMethods(
+      ["clientLoaded", "mount", "logout", "redirectEditUser"],
+      this
+    );
+    this.dataStore = new DataStore();
+    this.client = new CitrusClient();
+    this.Header = new Header(this.dataStore);
+  }
 
-    async addName(){
-        const nameStored = this.dataStore.get("name"); 
-        const nameElement = document.getElementById("name"); 
-        console.log(nameStored);
-        console.log(nameElement);
+  async clientLoaded() {
+    const identity = await this.client.getIdentity();
+    const user = await this.client.getUser(identity.email);
+    const fName = user.userModel.firstName;
+    const lName = user.userModel.lastName;
+    console.log(fName);
+    console.log(lName);
+    const email = identity.email;
+    const gender = user.userModel.gender;
+    const dob = user.userModel.dateOfBirth;
+    const userInterests = user.userModel.interests;
+    this.dataStore.set("email", email);
+    this.dataStore.set("firstName",fName);
+    this.dataStore.set("lastName",lName);
+    this.dataStore.set("interests", userInterests);
+    this.dataStore.set("gender", gender);
+    this.dataStore.set("dateOfBirth", dob);
+  }
 
-        if (nameStored == null) {
-            nameElement.innerText = "John Doe";
-        } else {
-            nameElement.innerText = nameStored;
-        }
-}
-    async addGender() {
-        const gender = this.dataStore.get("gender");
-        const genderElement = document.getElementById("gender");
-        
-        if (gender == null) {
-        genderElement.innerText = "Gender";
-        } else {
-        genderElement.innerText = gender;
-        }
-    }
-    async addInterests() {
-        const interests = this.dataStore.get("interests");
-        const interestsElement = document.getElementById("userInterests");
+  mount() {
+    this.clientLoaded().then(() => {
+      this.addName();
+      this.addEmail();
+      this.addInterests();
+      this.addGender();
+      this.addDateOfBirth();
+    });
     
-        if (interests == null || interests.length === 0) { // Check if interests array is empty
-        interestsElement.innerText = "Add your interests here!";
-        } else {
-        const interestsList = document.createElement("ul");
-    
-        interests.forEach((interest) => {
-            const listItem = document.createElement("li");
-            listItem.innerText = interest;
-            interestsList.appendChild(listItem);
-        });
-    
-        interestsElement.appendChild(interestsList);
-        }
-    }
+    document.getElementById("logout").addEventListener("click", this.logout);
+    document.getElementById("editProfileButton").addEventListener("click", this.redirectEditUser);
+  }
+
+  async addName() {
+    const firstName = this.dataStore.get("firstName");
+    const lastName = this.dataStore.get("lastName");
+    const nameElement = document.getElementById("name");
   
-    async addDateOfBirth(){
-        const dob = this.dataStore.get("dateOfBirth");
-        const dobElement = document.getElementById("dateOfBirth");
-        
-        if (dob == null) {
-        dobElement.innerText = "DateOfBirth";
-        } else {
-        dobElement.innerText = dob;
-        }
+    if (firstName == null || lastName == null) {
+      nameElement.innerText = "John Doe";
+    } else {
+      nameElement.innerText = `${firstName} ${lastName}`;
     }
-  
-    async addEmail(){
-        const emailStored = this.dataStore.get("email");
-        const emailElement = document.getElementById("email");
-        console.log(emailStored);
-        console.log(emailElement);
+  }
+  async addGender() {
+    const gender = this.dataStore.get("gender");
+    const genderElement = document.getElementById("gender");
 
-        if(emailStored == null){
-            emailElement.innerText = "info@example.com"
-        } else {
-            emailElement.innerText = emailStored;
-        }
+    if (gender == null) {
+      genderElement.innerText = "Gender";
+    } else {
+      genderElement.innerText = gender;
     }
-    async logout(){
-            await this.client.logout();
-            if(!this.client.isLoggedIn()){
-                window.location.href ='/landingPage.html';
-            }
-        }
-    
-    async redirectEditUser() {
-        window.location.href = '/editUser.html';
-        console.log("Edit profile button");
+  }
+
+  async addInterests() {
+    const interests = this.dataStore.get("interests");
+    const interestsElement = document.getElementById("userInterests");
+
+    if (interests == null || interests.length === 0) {
+      interestsElement.innerText = "Add your interests here!";
+    } else {
+      const interestsList = document.createElement("ul");
+
+      interests.forEach((interest) => {
+        const listItem = document.createElement("li");
+        listItem.innerText = interest;
+        interestsList.appendChild(listItem);
+      });
+
+      interestsElement.appendChild(interestsList);
     }
+  }
+
+  async addDateOfBirth() {
+    const dob = this.dataStore.get("dateOfBirth");
+    const dobElement = document.getElementById("dateOfBirth");
+
+    if (dob == null) {
+      dobElement.innerText = "DateOfBirth";
+    } else {
+      dobElement.innerText = dob;
+    }
+  }
+
+  async addEmail() {
+    const emailStored = this.dataStore.get("email");
+    const emailElement = document.getElementById("email");
+
+    if (emailStored == null) {
+      emailElement.innerText = "info@example.com";
+    } else {
+      emailElement.innerText = emailStored;
+    }
+  }
+
+  async logout() {
+    await this.client.logout();
+    if (!this.client.isLoggedIn()) {
+      window.location.href = "/landingPage.html";
+    }
+  }
+
+  async redirectEditUser() {
+    window.location.href = "/editUser.html";
+  }
 }
-        const main = async () => {
-            const viewUser = new ViewUser();
-            viewUser.mount();
-        };
-        
-        window.addEventListener('DOMContentLoaded', main)
 
+const main = async () => {
+  const viewUser = new ViewUser();
+  viewUser.mount();
+};
+
+window.addEventListener("DOMContentLoaded", main);
